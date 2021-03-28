b692060960e4bbbdb46040289a7dcb56ad931174
Mike Burgess <mddburgess@users.noreply.github.com>
2021-03-28 14:38:51 -0700

    Refactor API calls.

--- frontend/src/api/backlogs.ts
+++ frontend/src/api/backlogs.ts
@@ -2,28 +2,26 @@ import Axios from "axios";
 import { Backlog } from "../types/Backlog";
 import { Title } from "../types/Title";
 
-const listBacklogs = async (): Promise<Backlog[]> => {
-  const response = await Axios.get<Backlog[]>("/api/backlogs");
-  return response.data;
+const backlogs = {
+  list: async (): Promise<Backlog[]> => {
+    const response = await Axios.get<Backlog[]>("/api/backlogs");
+    return response.data;
+  },
+
+  create: async (title: Title): Promise<Backlog> => {
+    const request = {
+      title: {
+        key: title.key,
+      },
+    };
+    const response = await Axios.post<Backlog>("/api/backlogs", request);
+    return response.data;
+  },
+
+  delete: async (backlog: Backlog): Promise<string | undefined> => {
+    await Axios.delete(`/api/backlogs/${backlog.key}`);
+    return backlog.key;
+  },
 };
 
-const createBacklog = async (title: Title): Promise<Backlog> => {
-  const request = {
-    title: {
-      key: title.key,
-    },
-  };
-  const response = await Axios.post<Backlog>("/api/backlogs", request);
-  return response.data;
-};
-
-const deleteBacklog = async (backlog: Backlog): Promise<string | undefined> => {
-  await Axios.delete(`/api/backlogs/${backlog.key}`);
-  return backlog.key;
-};
-
-export const backlogs = {
-  list: listBacklogs,
-  create: createBacklog,
-  delete: deleteBacklog,
-};
+export default backlogs;
